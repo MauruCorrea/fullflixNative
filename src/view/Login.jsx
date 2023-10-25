a7c2c8a530294603d31821292f585374ca5d86f3
Mauricius <mauru.cor@gmail.com>
2023-10-25 17:50:04 -0300

    [Login] add Style

--- src/view/Login.jsx
+++ src/view/Login.jsx
@@ -1,19 +1,19 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { Text } from 'react-native';
 
 const Login = () => {
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView>
-      <Text>Login</Text>
-      <TextInput placeholder='ID'></TextInput>
-      <TextInput placeholder='PASSWORD'></TextInput>
+    <SafeAreaView style={style.container}>
+      <Text style={style.titleLogin}>Login</Text>
+      <TextInput style={style.input} placeholder='ID:       '></TextInput>
+      <TextInput style={style.input} placeholder='PASSWORD: '></TextInput>
 
       <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-        <Text>ACESSAR</Text>
+        <Text style={style.btnAccess}>ACESSAR</Text>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => navigation.navigate('Registre')}>
@@ -24,3 +24,32 @@ const Login = () => {
 };
 
 export default Login;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d0d0d0',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleLogin: {
+    fontSize: 32,
+    fontWeight: '600',
+  },
+  input: {
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    borderWidth: 1,
+    paddingHorizontal: 100,
+    paddingVertical: 8,
+    margin: 4,
+  },
+  btnAccess: {
+    backgroundColor: 'red',
+    borderRadius: 5,
+    paddingHorizontal: 15,
+    paddingVertical: 3,
+    color: '#fff',
+    margin: 10,
+  },
+});
