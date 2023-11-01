3012f07009eca0ee79f22960ba4c8a284168a1d6
Mauricius <mauru.cor@gmail.com>
2023-11-01 11:56:13 -0300

    [update] details

--- src/assets/api/Req.js
+++ src/assets/api/Req.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
 //Env
-const apiKey ='87e1e2afc9747ccde6f65bfc167be302';
+export const apiKey ='87e1e2afc9747ccde6f65bfc167be302';
 
-export const movieListApi = axios.create({
-  baseURL: `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
-  // baseURL: `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt&page=1`
-})
-export const movieDetailsApi = axios.create({
+export const api = axios.create({
   baseURL: `https://api.themoviedb.org/3/movie/`
   // baseURL: `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt&page=1`
 })
--- src/view/Details.jsx
+++ src/view/Details.jsx
@@ -25,7 +25,7 @@ const Details = () => {
 
       setMovieDetails(result.data);
     } catch (error) {
-      console.error(`Details error`);
+      console.error(`Error to get details, error`);
     }
   }
 
